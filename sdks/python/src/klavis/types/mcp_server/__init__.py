915e426b126c369216b202fc8284bdde27f9d6bf
Zihao Lin <zihaolin@klavis.ai>
2025-06-19 09:01:19 -0700

    feat/use fern for sdk

--- sdks/python/src/klavis/types/mcp_server/__init__.py
+++
@@ -1,9 +0,0 @@
-# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-from __future__ import annotations
-
-from .status_response import StatusResponse as StatusResponse
-from .instance_create_params import InstanceCreateParams as InstanceCreateParams
-from .instance_create_response import InstanceCreateResponse as InstanceCreateResponse
-from .instance_retrieve_response import InstanceRetrieveResponse as InstanceRetrieveResponse
-from .instance_set_auth_token_params import InstanceSetAuthTokenParams as InstanceSetAuthTokenParams
